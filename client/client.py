afb008d6e1ba35bc9fdfe6e0b52d566386fd6e1e
LogicAL007 <ayomidemtsh@gmail.com>
2024-01-12 22:21:31 +0100

    write properly

--- client/client.py
+++ client/client.py
@@ -1,24 +1,28 @@
+import os
 import pandas as pd
 import requests
-import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
+def send_json_data(customers_data, URL):
+    """
+    Send customer data as JSON to an API endpoint.
 
-URL = os.getenv("API_URL")
-FILE_PATH = os.getenv('CSV_PATH')
+    :param URL: this API address for the post request
+    :param customers_data: DataFrame containing customer data.
 
-# read the testfile
-customer_data = pd.read_csv(FILE_PATH)
+    Raises:
+    Exception: If there's an error while sending data to the API.
 
-def send_json_data(customer_data):
-    for i in customer_data.index:
+    Returns:
+    None
+    """
+    for i in customers_data.index:
         try:
             # convert the row to json
-            export = customer_data.loc[i].to_json()
+            export = customers_data.loc[i].to_json()
 
-            # send it to the api
+            # send it to the API
             response = requests.post(URL, data=export)
 
             # print the return code and response content
@@ -28,5 +32,15 @@ def send_json_data(customer_data):
         except Exception as e:
             print(f"Error sending data for row {i}: {str(e)}")
 
+
+def main():
+    load_dotenv()
+    URL = os.getenv("API_URL")
+    FILE_PATH = os.getenv('CSV_PATH')
+    customer_data = pd.read_csv(FILE_PATH)
+
+    send_json_data(customer_data, URL)
+
+
 if __name__ == '__main__':
-    send_json_data(customer_data)
+    main()
